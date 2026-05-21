0c61409557259f6a2dcf49d5d1810c6bcfa34a0a
malenagonzalez <malena.gonzalez.inf@gmail.com>
2026-05-21 12:02:52 -0300

    initial commit. Proyecto prog vang con Spring Boot 3.3.5

---
+++ src/test/java/com/codeaudit/orchestrator/CodeAuditOrchestratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codeaudit.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CodeAuditOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
