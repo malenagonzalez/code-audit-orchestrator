32aca3651aa3016b4b19393426d701b8d4381b8b
malenagonzalez <malena.gonzalez.inf@gmail.com>
2026-05-29 01:00:04 -0300

    se agrega UserDetailsServiceImpl y JwtAuthFilter. tmb se reemplaza SecutiryConfig con soporte JWT

---
+++ src/main/java/com/codeaudit/orchestrator/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.codeaudit.orchestrator.security;
+
+import com.codeaudit.orchestrator.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
