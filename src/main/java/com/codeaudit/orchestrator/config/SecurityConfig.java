9c3cd173eb660df428763d057d8ed19f6ca8139b
malenagonzalez <malena.gonzalez.inf@gmail.com>
2026-05-30 23:55:45 -0300

    modifico securityconfig

--- src/main/java/com/codeaudit/orchestrator/config/SecurityConfig.java
+++ src/main/java/com/codeaudit/orchestrator/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.codeaudit.orchestrator.config;
 
-import com.codeaudit.orchestrator.security.JwtAuthFilter;
+import com.auditoria.orchestrator.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
